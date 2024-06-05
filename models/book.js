ff46b247b75eb75769ad8fbae4b1140d41dcdf28
unknown <abhayagnihotri1585@gmail.com>
2024-06-05 10:26:01 +0530

    alterd-ui

--- models/book.js
+++ models/book.js
@@ -1,12 +1,12 @@
 const { Schema, model, default: mongoose } = require("mongoose");
 
 const bookSchema = new Schema({
-    title: { type: String, required: true },
-    coverImage: { type: String, required: true },
-    author: { type: String, required: true },
-    description: { type: String, required: true },
-    isbnNumber: { type: Number, required: true },
-    price: { type: Number, required: true },
+    title: { type: String, required: [true,"title is required"],trim:true,minlength : 5},
+    coverImage: { type: String, required: [true,"CoverImage is required"],trim:true},
+    author: { type: String, required: [true,"Author Name is Required"],trim:true },
+    description: { type: String, required: true,trim:true },
+    isbnNumber: { type: Number, required: true,trim:true },
+    price: { type: Number, required: true,trim:true },
 });
 
 const Book = model('Book', bookSchema);
